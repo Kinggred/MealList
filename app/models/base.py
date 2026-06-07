88acf479057bc39255f153b7892e1c63526d50dc
Maksymilian Grupiński <maks.grupinski@gmail.com>
2026-06-07 12:30:58 +0200

    Models setup

--- app/models/base.py
+++ app/models/base.py
@@ -1,18 +1,14 @@
 from datetime import datetime
 from uuid import UUID
 
-from sqlmodel import Field, SQLModel, func
-
-
-def generate_auto_model_name(model: str, created_at: datetime):
-    return f"New {model} {created_at.strftime('%Y-%m-%d')}"
+from sqlmodel import Field, SQLModel, func, text
 
 
 class BaseModel(SQLModel):
     id: UUID = Field(
         primary_key=True,
         sa_column_kwargs={
-            "server_default": "uuid_generate_v4()",
+            "server_default": text("uuid_generate_v4()"),
         },
     )
     created_at: datetime = Field(default_factory=func.now)
---
+++ app/models/ingredient_self_reference.py
@@ -0,0 +1,10 @@
+from sqlmodel import Field
+from uuid import UUID
+
+from app.models.base import BaseModel
+
+
+class IngredientSelfReference(BaseModel, table=True):
+    __tablename__ = 'ingredient_self_reference'
+    ingredient_id: UUID = Field(foreign_key="ingredient.id")
+    contained_id: UUID = Field(foreign_key="ingredient.id")
